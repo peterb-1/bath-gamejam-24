6b954b7f37ebea833ef3fb657259f3ff1fab30b2
Peter Ball <114352915+peterb-1@users.noreply.github.com>
2025-06-11 13:41:07 +0100

    Button borders, hologram animation when paused, fix 1-4 typo

---
+++ Assets/Scripts/UI/ShaderUnscaledTimeProvider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ShaderUnscaledTimeProvider : MonoBehaviour
+    {
+        [SerializeField] 
+        private Graphic targetGraphic;
+
+        private static readonly int UnscaledTime = Shader.PropertyToID("_UnscaledTime");
+
+        private void Awake()
+        {
+            if (targetGraphic.material == null)
+            {
+                Destroy(this);
+            }
+        }
+
+        private void Update()
+        {
+            targetGraphic.material.SetFloat(UnscaledTime, Time.unscaledTime);
+        }
+    }
+}
