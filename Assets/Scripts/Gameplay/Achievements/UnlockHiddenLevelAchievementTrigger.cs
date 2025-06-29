a9a50cad6c3fb02b9b6bc9c896fcece9bae1af9e
Jakub Bronowicki <jakub.bronowicki03@gmail.com>
2025-06-29 12:51:08 +0100

    Add Hidden Level Achievement
    :)

---
+++ Assets/Scripts/Gameplay/Achievements/UnlockHiddenLevelAchievementTrigger.cs
@@ -0,0 +1,27 @@
+using Core;
+using Core.Saving;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Gameplay.Achievements
+{
+    public class UnlockHiddenLevelAchievementTrigger : AbstractAchievementTrigger
+    {
+        private async void Awake()
+        {
+            await UniTask.WaitUntil(() => SaveManager.IsReady);
+
+            foreach (var sceneConfig in SceneLoader.Instance.SceneConfigs)
+            {
+                if (!sceneConfig.IsLevelScene) continue;
+                
+                if (SaveManager.Instance.SaveData.CampaignData.TryGetLevelData(sceneConfig.LevelConfig, out var levelData) && 
+                    levelData.IsUnlocked && 
+                    sceneConfig.LevelConfig.IsHidden)
+                {
+                    TriggerAchievement();
+                }
+            }
+        }
+    }
+}
