335730d505324fb6ac0fbf94c9edd8ab993acc36
Peter Ball <peterball.essex@live.com>
2026-01-03 22:37:59 +0000

    Add action to switch drone movement strategies

---
+++ Assets/Scripts/Gameplay/Events/SwitchDroneMovementStrategyAction.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using Gameplay.Drone;
+using UnityEngine;
+using Utils;
+
+namespace Gameplay.Events
+{
+    public class SwitchDroneMovementStrategyAction : AbstractEventAction
+    {
+        [SerializeField] 
+        private DroneMovementBehaviour drone;
+
+        [SerializeReference, SubclassSelector] 
+        private IDroneMovementStrategy newStrategy;
+        
+        public override UniTask Execute()
+        {
+            drone.SetMovementStrategy(newStrategy);
+            
+            return UniTask.CompletedTask;
+        }
+    }
+}
